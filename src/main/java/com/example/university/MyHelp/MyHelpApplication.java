99c13fa7e8ea3fbebb90875aaff5f166d0d5d8f1
Kacper Kingsford <kacper.kingsford@dynatrace.com>
2022-08-21 18:54:20 +0200

    debug for ECS

--- src/main/java/com/example/university/MyHelp/MyHelpApplication.java
+++ src/main/java/com/example/university/MyHelp/MyHelpApplication.java
@@ -17,6 +17,12 @@ public class MyHelpApplication {
 		SpringApplication.run(MyHelpApplication.class, args);
 		ApplicationContext context = new AnnotationConfigApplicationContext();
 		System.out.println("Active profiles: " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
+		System.out.println("db user: " + context.getEnvironment().getProperty("DB_USER"));
+		System.out.println("DB_PASSWORD: " + context.getEnvironment().getProperty("DB_PASSWORD"));
+		System.out.println("JWT_EXPIRATION_MS: " + context.getEnvironment().getProperty("JWT_EXPIRATION_MS"));
+		System.out.println("JWT_SECRET: " + context.getEnvironment().getProperty("JWT_SECRET"));
+		System.out.println("AWS_ACCESS_KEY: " + context.getEnvironment().getProperty("AWS_ACCESS_KEY"));
+		System.out.println("AWS_SECRET_KEY: " + context.getEnvironment().getProperty("AWS_SECRET_KEY"));
 	}
 
 }
